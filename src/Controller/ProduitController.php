e91c9f8cb96193389409842764f3f55aaaa8b167
Mayemba Eric <emayemba@SQLI51290.SQLI>
2020-08-06 13:37:32 +0200

    Twig produit et type ainsi que les ocntrolleurs

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,22 @@ class ProduitController extends AbstractController
     {
         return $this->render('produit/index.html.twig', [
             'produits' => $produitRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @param ProduitRepository $produitRepository
+     * @param Produit $produit
+     * @Route("/show/{id}",name="produit_show")
+     * Function show
+     * User: emayemba
+     * Date: 06/08/2020
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(ProduitRepository $produitRepository,Produit $produit){
+
+        return $this->render('produit/showProduit.html.twig',[
+            'produit'=>$produit,
         ]);
     }
 }
--- src/Controller/TypeController.php
+++ src/Controller/TypeController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Type;
 use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/type")
+ **/
 class TypeController extends AbstractController
 {
     /**
-     * @Route("/type", name="type")
+     * @Route("/", name="type")
      */
     public function index(TypeRepository $typeRepository)
     {
         return $this->render('type/index.html.twig', [
             'types' => $typeRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @param TypeRepository $typeRepository
+     * @param Type $type
+     * @Route("/show/{id}",name="type_show")
+     * Function show
+     * User: emayemba
+     * Date: 06/08/2020
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(TypeRepository $typeRepository,Type $type)
+    {
+        return $this->render('type/showType.html.twig',[
+            'type'=>$type,
         ]);
     }
 }
